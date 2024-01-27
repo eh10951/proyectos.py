f25e7c0d907a66db22ac2b1aaf9d637eebbe0e3f
HENRY_PY7 <111930922+eh10951@users.noreply.github.com>
2024-01-27 16:05:24 -0600

    Add files via upload

---
+++ if_elif_else.py
@@ -0,0 +1,41 @@
+
+# hacer un programa que me permita saber si una persona tiene la edad necesaria en el rango de +18 y saber si no; "and-or"
+  
+#   edad=15print (edad > 18 or edad < 30) 
+
+
+
+
+
+
+
+
+temperatura = float (input ("ingrese temperatura:"))
+
+escala= input ("es farenheit(F) o celsius (C)?:").lower()
+
+
+if escala == "f":
+    celsius= (temperatura - 32) * 5/9
+    print (celsius)
+elif escala == "c": 
+    farenheit= temperatura * 1.8 + 32
+    print (farenheit)
+else: print ("escala incorrecta ingresa C para celsius o F para farenheit")
+
+
+
+lenguajes  = ["python","javascript","ruby","php"];
+print (lenguajes)
+
+
+
+
+
+
+
+
+
+
+
+
